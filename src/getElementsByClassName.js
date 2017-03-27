be4c15e9cb1c0fd34f0096967bdeee39d2cefe8e
Hack Reactor Student <student@hackreactor.com>
2017-03-27 12:17:34 -0700

    Completed function for getElementsByClassName
    Used recursion to complete writing the function for getElementsByClassName that passes the test.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,35 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // create array to store matching className elements
+  var output = [];
+
+  // starts search function
+  var search = function(element) {
+  	var nodes = element.childNodes;
+
+  	for (var i = 0; i < nodes.length; i++) {
+  	  console.log(nodes[i].classList);
+  	  if (nodes[i].classList !== undefined && nodes[i].classList.contains(className)) {
+  	  	output.push(nodes[i]);
+  	  }
+  	  
+  	  if (nodes[i].childNodes.length >= 1) {
+  	    search(nodes[i]);
+  	  }
+  	}
+  }
+  //console.log(output);
+  search(document);
+  return output;
+  	// iterates over elements
+  	  // if element = className
+  	    // save element in array
+  	  // if element has children
+  	    // call function on elements children
+
+
+  // run search function on document
+  // return search function result on document.body
+
 };
