57ba94d1e5a2818baa76858809ede6392b0a6581
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:34:42 -0700

    Completed stringifyJSON function and tests
    Edited test to update FILL_ME_IN to meaningful value. And completed
    the function itself to pass the tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+  	return ''+obj;
+  }
+
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+  	var result = [];
+  	for (var i = 0; i < obj.length; i++) {
+  	  result.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + result.join(',') + ']';
+  }
+
+
+  if (typeof obj === 'object') {
+  	var objOutput = [];
+  	for (var prop in obj) {
+  	  if (typeof obj[prop] === 'function' || obj[prop] === undefined) {
+  	  	continue;
+  	  }
+  	  else {
+		objOutput.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+  	  }
+  	}
+  	return '{' + objOutput.join(',') + '}';
+  }
 };
