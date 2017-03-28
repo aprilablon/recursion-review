ca6fd4450e946ae5e36135516530e38bc21e9399
Hack Reactor Student <student@hackreactor.com>
2017-03-27 20:07:05 -0700

    Changed implementation of parseJSON
    As key names may have commas in them, our original implementation was
    no longer valid. Attempting to write to version that passes this test,
    still in progress, but getting there.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,56 +7,117 @@ var parseJSON = function(json) {
   var NUMBERS = ['0','1','2','3','4','5','6','7','8','9','.','-'];
   var nums = json.split('');
   var condition = nums.every(function(x) {
-  	return NUMBERS.includes(x);
+    return NUMBERS.includes(x);
   });
   
   if (json === 'true') {
-  	return true;
+    return true;
   }
   else if (json === 'false') {
-  	return false;
+    return false;
   }
   else if (json === 'undefined') {
-  	return undefined;
+    return undefined;
   }
   else if (json === 'null') {
-  	return null;
+    return null;
   }
   else if (json[0] === '"' && json[json.length - 1] === '"') {
-  	return json.slice(1, json.length - 1);
+    return json.slice(1, json.length - 1);
   }
   else if (condition) {
-  	return parseFloat(json);
+    return parseFloat(json);
   }
   else if (json === '[]') {
     return [];
   }
+  
   else if (json[0] === '[' && json[json.length - 1] === ']') {
-  	var arrayOutput = [];
-  	var arrayContent = json.slice(1, json.length - 1).split(',');
-  	for (var i = 0; i < arrayContent.length; i++) {
-  		arrayOutput.push(parseJSON(arrayContent[i]));
-  		
-  	}
-  	return arrayOutput;
+    var arrayOutput = [];
+    var arrayContent = json.slice(1, json.length - 1).split(',');
+    for (var i = 0; i < arrayContent.length; i++) {
+      arrayOutput.push(parseJSON(arrayContent[i]));
+      
+    }
+    return arrayOutput;
   }
+  
+  // object
   else if (json === '{}') {
     return {};
   }
+  // else if (json[0] === '{' && json[json.length - 1] === '}') {
+  //  var objOutput = {};
+  //  var objContent = json.slice(1, json.length - 1).split(', ');
+  //  for (var j = 0; j < objContent.length; j++) {
+  //    var colon = objContent[j].indexOf(':');
+  //    var key = parseJSON(objContent[j].slice(0, colon));
+  //    var value = parseJSON(objContent[j].slice(colon + 2));
+  //    console.log(objContent[j].slice(colon + 1));
+  //    objOutput[key] = value;
+  //  }
+  //  return objOutput;
+  // }
+
   else if (json[0] === '{' && json[json.length - 1] === '}') {
-  	var objOutput = {};
-  	var objContent = json.slice(1, json.length - 1).split(', ');
-  	for (var j = 0; j < objContent.length; j++) {
-  	  var colon = objContent[j].indexOf(':');
-  	  var key = parseJSON(objContent[j].slice(0, colon));
-  	  var value = parseJSON(objContent[j].slice(colon + 2));
-  	  console.log(objContent[j].slice(colon + 1));
-  	  objOutput[key] = value;
-  	}
-  	return objOutput;
+    var objOutput = {};
+    
+    var objContent = json.slice(1, json.length - 1);
+
+    var count = 0;
+
+    for (var j = 0; j < objContent.length; j++) {
+      if (objContent[j] === '"') {
+        count++;
+      }
+    }
+
+    var start = 0;
+    var end, key, property
+    var remainder = objContent.slice(1);
+
+    // init store start quote index
+    for (var k = 0; k < count; k++) {
+      end = remainder.indexOf('"') + (objContent.length - remainder.length);
+      if (key === undefined) {
+        var test = objContent.slice(start + 1, end); 
+        if (test !== ':') {
+          key = test;
+        }
+      } 
+      else {
+        var test2 = objContent.slice(start + 1, end).trim(); 
+        console.log(test2)
+        if (test2 + '' !== ':') {
+          property = test2;
+          console.log(property);
+          if (property[property.length - 1] === ',') {
+            var property = property.slice(0, property.length - 1);
+          }
+          console.log(property);
+          objOutput[key] = property;
+          key = undefined;
+        }
+      }
+      start = end;
+      remainder = objContent.slice(end + 1);
+    }
+
+    // init store end quote index
+    // init store key
+    // init store property
+    // init store remainder of string
+
+    // loop
+      // update
+
+
+    return objOutput;
   }
+
+
   // else {
-  // 	throw new SyntaxError();
+  //  throw new SyntaxError();
   // }
 };
 
